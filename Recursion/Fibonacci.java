274da2908a5826eb955775b6417ebaee8d1e1377
Jasmine-21 <jasmine171353.cse@chitkara.edu.in>
2020-01-08 22:20:31 +0530

    Revision

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,17 @@
+package Recursion;
+//0 1 1 2 3 5 8 13 21 34 55
+public class Fibonacci {
+	static int print(int n)
+	{
+		if(n<=1)
+			return n;
+		//print(n-1)+print(n-2);
+		return print(n-1)+print(n-2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print(print(9));
+	}
+
+}
