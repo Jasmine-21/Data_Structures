71ecfd84ee9c98b30bc6a292afc6d9f213cd0787
Jasmine-21 <jasmine171353.cse@chitkara.edu.in>
2020-01-08 22:23:36 +0530

    Arrays

--- Arrays/Sum_Of_two_Jagged_Array.java
+++ Arrays/Sum_Of_two_Jagged_Array.java
@@ -5,24 +5,44 @@ package Arrays;
  *  These type of arrays are also known as Jagged arrays.
  */
 public class Sum_Of_two_Jagged_Array {
-public static void main(String[] args) {
-	int r = 5; 
-    int arr[][] = new int[8][]; 
- // Creating a 2D array such that first row 
-    // has 1 element, second row has two  
-    // elements and so on.
-    for (int i=0; i<arr.length; i++) 
-        arr[i] = new int[i+1]; 
-    int count = 0; 
-    for (int i=0; i<arr.length; i++) 
-        for(int j=0; j<arr[i].length; j++) 
-            arr[i][j] = count++; 
-    System.out.println(" 2D Jagged Array"); 
-    for (int i=0; i<arr.length; i++) 
-    { 
-        for (int j=0; j<arr[i].length; j++) 
-            System.out.print(arr[i][j] + " "); 
-        System.out.println(); 
-    } 
-	}
+	public static void main (String args[])
+    {
+        int count=0;
+        int a[][]=new int[5][];
+        int b[][]=new int[5][];
+        int c[][]=new int[5][];
+        for(int i=0;i<5;i++) 
+        {
+            a[i]=new int [i+1];
+            b[i]=new int [i+1];
+            c[i]=new int [i+1];
+        } 
+        //rows
+        for(int i=0;i<5;i++)
+        {
+        	//coulmns
+            for(int j=0;j<a[i].length;j++)
+            { 
+                 
+                a[i][j]=count++;
+                b[i][j]=count++;
+               
+            }
+        }
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<a[i].length;j++)
+            {
+                c[i][j] = b[i][j]+a[i][j];
+            }
+        }
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<c[i].length;j++)
+            {
+              System.out.print(c[i][j]+" ")  ;
+                }
+                System.out.println();
+        }
+    }
 }
